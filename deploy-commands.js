752f4dc5847296b77c8a505149f1ba5f9402d6af
deirdrahyland-oss <deirdrahyland@gmail.com>
2026-06-19 20:29:48 -0400

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Test bot connection")
+    .toJSON()
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("Slash commands registered.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
