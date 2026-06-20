46e8c7cfdcc589ea4750baae2564e2fe3ca8338d
deirdrahyland-oss <deirdrahyland@gmail.com>
2026-06-19 20:43:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 require("dotenv").config();
 
-const {
-  Client,
-  GatewayIntentBits
-} = require("discord.js");
+const { Client, GatewayIntentBits, Events } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on("interactionCreate", async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async interaction => {
+  try {
+    if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
+    console.log("Command received:", interaction.commandName);
+
+    if (interaction.commandName === "ping") {
+      await interaction.reply("Pong!");
+    }
+  } catch (error) {
+    console.error("Interaction Error:", error);
   }
 });
 
