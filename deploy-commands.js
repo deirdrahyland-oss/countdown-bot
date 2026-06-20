c5d026530f232709af8c4254525bf87ac0bafa0d
deirdrahyland-oss <deirdrahyland@gmail.com>
2026-06-19 21:17:25 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+
 const commands = [
 
   new SlashCommandBuilder()
@@ -49,3 +53,21 @@ const commands = [
     .setDescription("Cancel active countdown")
 
 ].map(command => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log("Slash commands registered.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
