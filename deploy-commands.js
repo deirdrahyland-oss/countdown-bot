3d1cce15273abb23231928c469435a02e5070edf
deirdrahyland-oss <deirdrahyland@gmail.com>
2026-06-19 20:33:42 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,11 +13,13 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
-
+await rest.put(
+  Routes.applicationGuildCommands(
+    process.env.CLIENT_ID,
+    process.env.GUILD_ID
+  ),
+  { body: commands }
+);
     console.log("Slash commands registered.");
   } catch (error) {
     console.error(error);
